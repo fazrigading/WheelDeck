f1b4b6695f4e2b4181c02c93487cd08ccd824087
Fazri Gading <fazrigading@gmail.com>
2026-09-08 20:06:15 +0800

    fix: remove colorMode attribute from AndroidManifest and refactor MainActivity

--- mobile/android/app/src/main/kotlin/dev/fazrigading/wheeldeck/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/dev/fazrigading/wheeldeck/MainActivity.kt
@@ -1,5 +1,15 @@
 package dev.fazrigading.wheeldeck
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.RenderMode
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.attributes.colorMode = 1 // COLOR_MODE_SRGB
+        }
+    }
+
+    override fun getRenderMode(): RenderMode = RenderMode.texture
+}
